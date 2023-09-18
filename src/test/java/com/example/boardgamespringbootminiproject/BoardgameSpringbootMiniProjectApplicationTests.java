3a4283f4177914c49e081d818acfe930a56429a2
Elizabeth Yang <elizabethyang928@gmail.com>
2023-09-18 16:46:37 -0400

    First Commit

---
+++ src/test/java/com/example/boardgamespringbootminiproject/BoardgameSpringbootMiniProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boardgamespringbootminiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardgameSpringbootMiniProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
