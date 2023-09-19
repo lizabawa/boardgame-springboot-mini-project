960f5cc68a770f4b272e6fbea97dbca0e3d4bc37
Elizabeth Yang <elizabethyang928@gmail.com>
2023-09-19 11:55:52 -0400

    Create MyUserDetailsService class to security package

---
+++ src/main/java/com/example/boardgamespringbootminiproject/security/MyUserDetailsService.java
@@ -0,0 +1,11 @@
+package com.example.boardgamespringbootminiproject.security;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * This class finds the user via their email to gain permissions details about user
+ */
+@Service
+public class MyUserDetailsService {
+
+}
