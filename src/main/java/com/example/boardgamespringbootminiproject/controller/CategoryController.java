78c77915e8cfd35440e62b916b8bd44081d20ba6
Elizabeth Yang <elizabethyang928@gmail.com>
2023-09-19 10:25:33 -0400

    Autowire CategoryRepository to CategoryController

--- src/main/java/com/example/boardgamespringbootminiproject/controller/CategoryController.java
+++ src/main/java/com/example/boardgamespringbootminiproject/controller/CategoryController.java
@@ -1,5 +1,7 @@
 package com.example.boardgamespringbootminiproject.controller;
 
+import com.example.boardgamespringbootminiproject.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api") //http://localhost:9094/api
 public class CategoryController {
 
+private CategoryRepository categoryRepository;
 
-
+    @Autowired
+    public void setCategoryRepository(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
 }
