56e4b9b69c0afac5adda4bc781c31bf9ab8477f9
Elizabeth Yang <elizabethyang928@gmail.com>
2023-09-19 11:58:15 -0400

    Create UserService class in service package

--- src/main/java/com/example/boardgamespringbootminiproject/security/MyUserDetailsService.java
+++ src/main/java/com/example/boardgamespringbootminiproject/security/MyUserDetailsService.java
@@ -1,11 +1,18 @@
 package com.example.boardgamespringbootminiproject.security;
 
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 /**
  * This class finds the user via their email to gain permissions details about user
  */
 @Service
-public class MyUserDetailsService {
+public class MyUserDetailsService implements UserDetailsService {
 
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
 }
