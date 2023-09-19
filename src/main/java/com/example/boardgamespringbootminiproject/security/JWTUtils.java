f18519907cec16932a01c1c0838d53390ee3cb87
Elizabeth Yang <elizabethyang928@gmail.com>
2023-09-19 11:41:28 -0400

    Add jwt secret and expiration to JWTUtils class

--- src/main/java/com/example/boardgamespringbootminiproject/security/JWTUtils.java
+++ src/main/java/com/example/boardgamespringbootminiproject/security/JWTUtils.java
@@ -1,7 +1,13 @@
 package com.example.boardgamespringbootminiproject.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JWTUtils {
+    @Value("${jwt-secret}")
+    private String jwtSecret;
+
+    @Value("${jwt-expiration-ms}")
+    private int jwtExpirationMS;
 }
