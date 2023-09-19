84f70175749514cefe1149f47e18516174e0c796
Elizabeth Yang <elizabethyang928@gmail.com>
2023-09-19 12:22:14 -0400

    Add parseJwt and doFilterInternal methods to JwtRequestFilter class

--- src/main/java/com/example/boardgamespringbootminiproject/security/JwtRequestFilter.java
+++ src/main/java/com/example/boardgamespringbootminiproject/security/JwtRequestFilter.java
@@ -1,7 +1,12 @@
 package com.example.boardgamespringbootminiproject.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -31,8 +36,30 @@ public class JwtRequestFilter extends OncePerRequestFilter {
         this.jwtUtils = jwtUtils;
     }
 
+    private String parseJwt(HttpServletRequest request){
+        String headerAuth = request.getHeader("Authorization"); //when user sends a request, this checks for keycode header "authorization"
+        if (StringUtils.hasLength(headerAuth) && headerAuth.startsWith("Bearer")) {
+            return headerAuth.substring(7); //only returning Bearer
+        } else {
+            logger.info("No header");
+            return null; //unable to grab token returns null
+        }
+    }
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-
+        try{
+            String jwt = parseJwt(request);
+            if(jwt !=null && jwtUtils.validateJwtToken(jwt)){
+                String username = jwtUtils.getUserNameFromJwtToken(jwt);
+                UserDetails userDetails = this.myUserDetailsService.loadUserByUsername(username); //load user by email address
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities()); // performs authentication
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request)); //allows user access to the resource if user has correct permissions
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
+        } catch (Exception e) {
+            logger.info("Cannot set user authentication token"); //throws exception if unable to authenticate user
+        }
+        filterChain.doFilter(request, response);
     }
 }
