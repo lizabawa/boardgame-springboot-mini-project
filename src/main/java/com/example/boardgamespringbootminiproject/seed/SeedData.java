fd84712aa099a9225f6c301ba437441c3a745f98
Elizabeth Yang <elizabethyang928@gmail.com>
2023-09-19 12:46:04 -0400

    Create BoardgameRepository

--- src/main/java/com/example/boardgamespringbootminiproject/seed/SeedData.java
+++ src/main/java/com/example/boardgamespringbootminiproject/seed/SeedData.java
@@ -1,7 +1,16 @@
 package com.example.boardgamespringbootminiproject.seed;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+/**
+ * This class is for testing and not for production.
+ */
 @Component
-public class SeedData {
+public class SeedData implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }
