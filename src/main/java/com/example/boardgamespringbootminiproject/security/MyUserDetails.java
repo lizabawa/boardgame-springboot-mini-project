09ba0e019b268e60dc8290586692a35608e35de0
Elizabeth Yang <elizabethyang928@gmail.com>
2023-09-19 11:45:49 -0400

    Implement UserDetails to MyUserDetails class

--- src/main/java/com/example/boardgamespringbootminiproject/security/MyUserDetails.java
+++ src/main/java/com/example/boardgamespringbootminiproject/security/MyUserDetails.java
@@ -1,7 +1,58 @@
 package com.example.boardgamespringbootminiproject.security;
 
+import com.example.boardgamespringbootminiproject.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.HashSet;
+
 /**
  * This class checks if the user has the appropriate permissions or not
  */
-public class MyUserDetails {
+public class MyUserDetails implements UserDetails {
+    private final User user;
+
+    public MyUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return new HashSet<>();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmailAddress();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public User getUser() {
+        return user;
+    }
 }
