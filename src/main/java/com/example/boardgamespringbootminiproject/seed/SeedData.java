ae86c7504b26f864f3fa863a5bc21776630a624c
Elizabeth Yang <elizabethyang928@gmail.com>
2023-09-19 12:47:50 -0400

    Add constructors

--- src/main/java/com/example/boardgamespringbootminiproject/seed/SeedData.java
+++ src/main/java/com/example/boardgamespringbootminiproject/seed/SeedData.java
@@ -1,13 +1,33 @@
 package com.example.boardgamespringbootminiproject.seed;
 
+import com.example.boardgamespringbootminiproject.repository.BoardgameRepository;
+import com.example.boardgamespringbootminiproject.repository.CategoryRepository;
+import com.example.boardgamespringbootminiproject.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 /**
- * This class is for testing and not for production.
+ * This class is for storing data for testing purposes and should not be present for production.
  */
 @Component
 public class SeedData implements CommandLineRunner {
+
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final CategoryRepository categoryRepository;
+    private final BoardgameRepository boardgameRepository;
+
+    public SeedData(@Lazy PasswordEncoder passwordEncoder,
+                    UserRepository userRepository,
+                    CategoryRepository categoryRepository,
+                    BoardgameRepository boardgameRepository) {
+        this.passwordEncoder = passwordEncoder;
+        this.userRepository = userRepository;
+        this.categoryRepository = categoryRepository;
+        this.boardgameRepository = boardgameRepository;
+    }
 
     @Override
     public void run(String... args) throws Exception {
