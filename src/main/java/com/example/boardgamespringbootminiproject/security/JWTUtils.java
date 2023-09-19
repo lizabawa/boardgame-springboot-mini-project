73ba86ee86dc13407eb628fbabd9d5bab411792f
Elizabeth Yang <elizabethyang928@gmail.com>
2023-09-19 11:49:07 -0400

    Add generateJwtToken method to JWTUtils class

--- src/main/java/com/example/boardgamespringbootminiproject/security/JWTUtils.java
+++ src/main/java/com/example/boardgamespringbootminiproject/security/JWTUtils.java
@@ -1,7 +1,11 @@
 package com.example.boardgamespringbootminiproject.security;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 public class JWTUtils {
@@ -10,4 +14,14 @@ public class JWTUtils {
 
     @Value("${jwt-expiration-ms}")
     private int jwtExpirationMS;
+
+    //GENERATES A TOKEN ONCE USER LOGS IN
+    public String generateJwtToken(MyUserDetails myUserDetails){
+        return Jwts.builder()
+                .setSubject((myUserDetails.getUsername())) //get just the email address
+                .setIssuedAt(new Date()) //set time to whatever the current time is
+                .setExpiration(new Date(new Date().getTime() + jwtExpirationMS)) //sets expiration date to current time + the jwt expiration date
+                .signWith(SignatureAlgorithm.HS256, jwtSecret) //HS256 is the algorithm
+                .compact(); //compacts everything together
+    }
 }
