8d0a847f1ebfc5e0974fb66bc38927812060cbe4
Elizabeth Yang <elizabethyang928@gmail.com>
2023-09-19 14:53:34 -0400

    Add loginUser method to UserController and UserService

--- src/main/java/com/example/boardgamespringbootminiproject/controller/UserController.java
+++ src/main/java/com/example/boardgamespringbootminiproject/controller/UserController.java
@@ -2,12 +2,17 @@ package com.example.boardgamespringbootminiproject.controller;
 
 import com.example.boardgamespringbootminiproject.model.User;
 import com.example.boardgamespringbootminiproject.model.request.LoginRequest;
+import com.example.boardgamespringbootminiproject.model.response.LoginResponse;
 import com.example.boardgamespringbootminiproject.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/auth/users/") //http://localhost:9094/auth/users/
@@ -23,5 +28,16 @@ public class UserController {
     @PostMapping(path = "/register/") //http://localhost:9094/auth/users/register/
     public User createUser(@RequestBody User userObject){
         return userService.createUser(userObject);
+    }
+
+    //Login
+    @PostMapping(path = "/login/") //http://localhost:9094/auth/users/login/
+    public ResponseEntity<LoginResponse> loginUser(@RequestBody LoginRequest loginRequest) {
+        Optional<String> jwtToken = userService.loginUser(loginRequest); //authentication happens here, calling method from the UserService class
+        if(jwtToken.isPresent()){
+            return ResponseEntity.ok(new LoginResponse(jwtToken.get())); //ResponseEntity.ok is the 200 response
+        }else{
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new LoginResponse("Authentication failed")); //user not authenticated
+        }
     }
 }
--- src/main/java/com/example/boardgamespringbootminiproject/service/UserService.java
+++ src/main/java/com/example/boardgamespringbootminiproject/service/UserService.java
@@ -2,13 +2,20 @@ package com.example.boardgamespringbootminiproject.service;
 
 import com.example.boardgamespringbootminiproject.exception.InformationAlreadyExistsException;
 import com.example.boardgamespringbootminiproject.model.User;
+import com.example.boardgamespringbootminiproject.model.request.LoginRequest;
 import com.example.boardgamespringbootminiproject.repository.UserRepository;
 import com.example.boardgamespringbootminiproject.security.JWTUtils;
+import com.example.boardgamespringbootminiproject.security.MyUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -32,6 +39,21 @@ public class UserService {
             return userRepository.save(userObject);
         }else {
             throw new InformationAlreadyExistsException("A user with the email " + userObject.getEmailAddress() + " already exists.");
+        }
+    }
+
+    //LOGIN USER
+    public Optional<String> loginUser(LoginRequest loginRequest){
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(loginRequest.getEmailAddress(), loginRequest.getPassword()); //grab user login details (email and password)
+
+        try {
+            Authentication authentication = authenticationManager.authenticate(authenticationToken); //authenticating user based on the info grabbed
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            MyUserDetails myUserDetails = (MyUserDetails) authentication.getPrincipal();
+            return Optional.of(jwtUtils.generateJwtToken(myUserDetails));
+
+        }catch (Exception e){
+            return Optional.empty();
         }
     }
 
