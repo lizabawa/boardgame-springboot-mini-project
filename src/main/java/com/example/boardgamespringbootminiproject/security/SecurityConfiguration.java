927b2c437fb7748ffeee30ba5bc715992c97af1c
Elizabeth Yang <elizabethyang928@gmail.com>
2023-09-19 11:00:58 -0400

    Add passwordEncoder and authenticationmanager methods to SecurityConfiguration

--- src/main/java/com/example/boardgamespringbootminiproject/security/SecurityConfiguration.java
+++ src/main/java/com/example/boardgamespringbootminiproject/security/SecurityConfiguration.java
@@ -1,9 +1,23 @@
 package com.example.boardgamespringbootminiproject.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true) //must be authorized to gain access to endpoints
 public class SecurityConfiguration {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception{
+        return authConfig.getAuthenticationManager();
+    }
 }
