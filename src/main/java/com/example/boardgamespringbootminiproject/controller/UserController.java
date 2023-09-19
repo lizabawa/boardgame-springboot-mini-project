f798cb8b9fd0c8533a7a498a0905c2069ec8308c
Elizabeth Yang <elizabethyang928@gmail.com>
2023-09-19 13:20:24 -0400

    Autowire UserService in UserController class

--- src/main/java/com/example/boardgamespringbootminiproject/controller/UserController.java
+++ src/main/java/com/example/boardgamespringbootminiproject/controller/UserController.java
@@ -1,11 +1,17 @@
 package com.example.boardgamespringbootminiproject.controller;
 
 import com.example.boardgamespringbootminiproject.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/auth/users/") //http://localhost:9094/auth/users/
 public class UserController {
+    private UserService userService;
 
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
 }
