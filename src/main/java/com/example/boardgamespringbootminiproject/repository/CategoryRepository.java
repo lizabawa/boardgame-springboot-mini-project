32e4d36656a8c13d71b4882192eed14e3aee9ead
Elizabeth Yang <elizabethyang928@gmail.com>
2023-10-12 17:30:20 -0400

    Add docstring to CategoryRepository

--- src/main/java/com/example/boardgamespringbootminiproject/repository/CategoryRepository.java
+++ src/main/java/com/example/boardgamespringbootminiproject/repository/CategoryRepository.java
@@ -7,16 +7,43 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+/**
+ * The repository interface for managing Category entities in the database.
+ */
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
+    /**
+     * Finds and retrieves a list of categories associated with a specific user.
+     *
+     * @param userId The ID of the user for whom to retrieve categories.
+     * @return A list of categories associated with the specified user.
+     */
     List<Category> findByUserId(Long userId);
 
-    //find by categoryId AND userId
+    /**
+     * Finds a category by its ID and the user's ID.
+     *
+     * @param categoryId The ID of the category to retrieve.
+     * @param userId The ID of the user to whom the category belongs.
+     * @return The category with the specified ID and associated with the specified user.
+     */
     Category findByIdAndUserId(Long categoryId, Long userId);
 
+    /**
+     * Finds a category by its name and the user's ID.
+     *
+     * @param categoryName The name of the category to retrieve.
+     * @param userId The ID of the user to whom the category belongs.
+     * @return The category with the specified name and associated with the specified user.
+     */
     Category findByNameAndUserId(String categoryName, Long userId);
 
+    /**
+     * Deletes a category by its ID.
+     *
+     * @param categoryId The ID of the category to delete.
+     */
     @Transactional
     void deleteById(Long categoryId);
 }
