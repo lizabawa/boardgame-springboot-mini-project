96aaadae9b2f92a4ac92f024009693702c760298
Elizabeth Yang <elizabethyang928@gmail.com>
2023-10-12 17:58:05 -0400

    add docstring to boardgamerepository and userrepository

--- src/main/java/com/example/boardgamespringbootminiproject/repository/BoardgameRepository.java
+++ src/main/java/com/example/boardgamespringbootminiproject/repository/BoardgameRepository.java
@@ -6,11 +6,35 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+/**
+ * A repository interface for accessing and managing board game data in the database.
+ */
 @Repository
 public interface BoardgameRepository extends JpaRepository<Boardgame, Long> {
+
+    /**
+     * Find a board game by name and user ID.
+     *
+     * @param boardgameName The name of the board game.
+     * @param userId The ID of the user associated with the board game.
+     * @return The board game with the specified name and user ID.
+     */
     Boardgame findByNameAndUserId(String boardgameName, Long userId);
 
+    /**
+     * Find all board games within a specific category.
+     *
+     * @param categoryId The ID of the category.
+     * @return A list of board games within the specified category.
+     */
     List<Boardgame> findAllByCategoryId(Long categoryId);
 
+    /**
+     * Find a board game by ID and user ID.
+     *
+     * @param boardgameId The ID of the board game.
+     * @param userId The ID of the user associated with the board game.
+     * @return The board game with the specified ID and user ID.
+     */
     Boardgame findByIdAndUserId(Long boardgameId, Long userId);
 }
--- src/main/java/com/example/boardgamespringbootminiproject/repository/UserRepository.java
+++ src/main/java/com/example/boardgamespringbootminiproject/repository/UserRepository.java
@@ -4,11 +4,25 @@ import com.example.boardgamespringbootminiproject.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * A repository interface for accessing and managing user data in the database.
+ */
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    //to register
+
+    /**
+     * Check if a user with the specified email address already exists.
+     *
+     * @param userEmailAddress The email address to check for existence.
+     * @return `true` if a user with the email address exists, `false` otherwise.
+     */
     boolean existsByEmailAddress(String userEmailAddress);
 
-    //to login
+    /**
+     * Find a user by their email address.
+     *
+     * @param emailAddress The email address of the user to find.
+     * @return The user with the specified email address, or `null` if not found.
+     */
     User findUserByEmailAddress(String emailAddress);
 }
