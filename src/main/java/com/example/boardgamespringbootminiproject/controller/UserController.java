e05c14b36e192ccbe6bf369da06805e8246162b2
Elizabeth Yang <elizabethyang928@gmail.com>
2023-10-12 17:37:24 -0400

    Add docstrings to UserController

--- src/main/java/com/example/boardgamespringbootminiproject/controller/UserController.java
+++ src/main/java/com/example/boardgamespringbootminiproject/controller/UserController.java
@@ -27,13 +27,24 @@ public class UserController {
         this.userService = userService;
     }
 
-    //REGISTER USER
+    /**
+     * Registers a new user.
+     *
+     * @param userObject The user object to register.
+     * @return The created user.
+     */
     @PostMapping(path = "/register/") //http://localhost:9094/auth/users/register/
     public User createUser(@RequestBody User userObject){
         return userService.createUser(userObject);
     }
 
-    //LOGIN USER
+    /**
+     * Logs in a user and provides an authentication token (JWT) upon successful authentication.
+     *
+     * @param loginRequest The login request object containing user credentials.
+     * @return A ResponseEntity containing the authentication token (JWT) upon successful login,
+     *         or an unauthorized response if authentication fails.
+     */
     @PostMapping(path = "/login/") //http://localhost:9094/auth/users/login/
     public ResponseEntity<LoginResponse> loginUser(@RequestBody LoginRequest loginRequest) {
         Optional<String> jwtToken = userService.loginUser(loginRequest); //authentication happens here, calling method from the UserService class
